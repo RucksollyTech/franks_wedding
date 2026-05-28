7c7487a5213b146fd0fb60730ea5069c69d18f85
Rucksolly <rucksolly.tech@gmail.com>
2026-05-28 03:55:30 +0200

    Meta again

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,32 +29,49 @@ const lato = Lato({
 //   description: "Join us in celebrating the union of Frank and Susan on October 3, 2026. Explore our wedding website for details, RSVP, and more!",
 // };
 
-export const metadata: Metadata = {
+// export const metadata: Metadata = {
+//   title: "Susan & Franklin — Saturday, 3rd October 2026",
+//   description:
+//     "Join us for the wedding of Susan & Franklin in Lagos, Nigeria — Saturday, 3rd October 2026.",
+//   openGraph: {
+//     title: "Susan & Franklin — Saturday, 3rd October 2026",
+//     description:
+//       "Join us for the wedding of Susan & Franklin in Lagos, Nigeria — Saturday, 3rd October 2026.",
+//     url: "https://franks-wedding.vercel.app",
+//     siteName: "Susan & Franklin Wedding",
+//     images: [
+//       {
+//         url: "https://franks-wedding.vercel.app/images/try3.png",
+//         width: 1200,
+//         height: 630,
+//         alt: "Susan and Franklin wedding invitation",
+//       },
+//     ],
+//     type: "website",
+//   },
+//   twitter: {
+//     card: "summary_large_image",
+//     title: "Susan & Franklin — Saturday, 3rd October 2026",
+//     description:
+//       "Join us for the wedding of Susan & Franklin in Lagos, Nigeria — Saturday, 3rd October 2026.",
+//     images: ["https://franks-wedding.vercel.app/images/try3.png"],
+//   },
+// };
+export const metadata = {
   title: "Susan & Franklin — Saturday, 3rd October 2026",
   description:
-    "Join us for the wedding of Susan & Franklin in Lagos, Nigeria — Saturday, 3rd October 2026.",
+    "Join us in celebrating the union of Frank and Susan on October 3rd, 2026.",
   openGraph: {
     title: "Susan & Franklin — Saturday, 3rd October 2026",
     description:
-      "Join us for the wedding of Susan & Franklin in Lagos, Nigeria — Saturday, 3rd October 2026.",
-    url: "https://franks-wedding.vercel.app",
-    siteName: "Susan & Franklin Wedding",
+      "Join us in celebrating the union of Frank and Susan on October 3rd, 2026.",
     images: [
       {
         url: "https://franks-wedding.vercel.app/images/try3.png",
         width: 1200,
         height: 630,
-        alt: "Susan and Franklin wedding invitation",
       },
     ],
-    type: "website",
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: "Susan & Franklin — Saturday, 3rd October 2026",
-    description:
-      "Join us for the wedding of Susan & Franklin in Lagos, Nigeria — Saturday, 3rd October 2026.",
-    images: ["https://franks-wedding.vercel.app/images/try3.png"],
   },
 };
 
