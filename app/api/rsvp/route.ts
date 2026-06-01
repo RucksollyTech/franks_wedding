5be06b6429891fafcebdcb58495522e5461c237f
Okenwa Anthony <rucksolly.tech@gmail.com>
2026-06-01 15:12:02 +0200

    nothing changed just updating vercel env

--- app/api/rsvp/route.ts
+++ app/api/rsvp/route.ts
@@ -3,6 +3,7 @@ import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
   try {
+    // 
     const body = await req.json();
 
     const auth = new google.auth.GoogleAuth({
